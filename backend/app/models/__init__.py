faec701aa587cb1c611f73171dbc798e5b7110f6
Aryan Sarswat <aryansarswat2000@gmail.com>
2026-09-19 18:00:12 -0700

    feat: add chat_sessions/chat_messages SQLModel tables

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,5 @@
+from app.models.chat import ChatMessageRecord, ChatSession
 from app.models.download_job import DownloadJob
 from app.models.downloaded_model import DownloadedModelRecord
 
-__all__ = ["DownloadJob", "DownloadedModelRecord"]
+__all__ = ["ChatMessageRecord", "ChatSession", "DownloadJob", "DownloadedModelRecord"]
